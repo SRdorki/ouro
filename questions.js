8bbfa1d9587e20320fce35ef33cc68dda6c84c75
SRdorki <128515609+SRdorki@users.noreply.github.com>
2023-04-26 20:37:56 -0300

    escola

---
+++ questions.js
@@ -0,0 +1,42 @@
+export default [
+    {
+      question: "Uma garrafa de vinho com a capacidade de um litro, cheia de ouro pesaria quantos quilos?",
+      answers: [
+        { option: "Mais de 19 quilos", correct: true },
+        { option: "Menos de 19 quilos", correct: false },
+        { option: "50 quilos", correct: false },
+      ],
+    },
+    {
+      question: "Ouro é simbolo de:",
+      answers: [
+        { option: "azar, tristeza, felicidade", correct: false },
+        { option: "sorte, felicidade, ostentação", correct: true },
+        { option: "família, riqueza, paz", correct: false },
+      ],
+    },
+    {
+      question: "Quantos tons de ouro existem?",
+      answers: [
+        { option: "31 tonss", correct: false },
+        { option: "15 tons", correct: false },
+        { option: "19 tons", correct: true },
+      ],
+    },
+    {
+      question: "Em algum lugar da América do Sul existia uma tribo de índios, cujo chefe se banhava em pó de ouro. Qual era o seu nome?",
+      answers: [
+        { option: "El Prata", correct: false },
+        { option: "El Bronze", correct: false },
+        { option: "El Dorado", correct: true },
+      ],
+    },
+    {
+    question: "O ouro provém de que?",
+    answers: [
+      { option: "Terra", correct: false },
+      { option: "Pedras", correct: true},
+      { option: "Cobre", correct: false },
+    ],
+   },
+];
